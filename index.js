f210fe2215a15c88f7dd44159f24d528bf3eab15
Juan Pablo Delgado <juan.delgado@onthefuze.com>
2025-01-20 16:12:42 -0500

    Integrating With HubSpot I: Foundations

--- index.js
+++ index.js
@@ -12,8 +12,22 @@ const PRIVATE_APP_ACCESS = process.env.TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-app.get("/",(req, res)=>{
-    res.render('homepage', { title: 'Home Page' });      
+app.get("/",async (req, res)=>{
+    const registers = 'https://api.hubspot.com/crm/v3/objects/p_video_games/?properties=role,video_game_name'
+    const headers = {        
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+
+try {
+    const resp = await axios.get(registers, {headers})
+    const data = resp.data.results
+    console.log(data);
+    
+    res.render('homepage', { title: 'Home Page', data });         
+} catch (error) {
+    console.error(error)
+}
 })
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
